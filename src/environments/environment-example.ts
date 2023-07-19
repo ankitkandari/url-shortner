38a38cd8208e8e5e5c6b1c803c9c0f295dd3eb13
Ankit Singh <codertacina@gmail.com>
2023-07-19 17:49:39 +0530

    update

---
+++ src/environments/environment-example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+      projectId: '',
+      appId: '',
+      storageBucket: '',
+      apiKey: '',
+      authDomain: '',
+      messagingSenderId: '',
+      measurementId: '',
+    },
+    base:'http://localhost:4200'
+  };
